5a68a6faae07d6221df01ecc6ace6ebf621d84c7
Will <will.franklind@gmail.com>
2025-03-07 15:38:01 +0000

    Take Auth0 params to use in package

--- src/Http/Middleware/Auth0AuthenticateMiddleware.php
+++ src/Http/Middleware/Auth0AuthenticateMiddleware.php
@@ -8,9 +8,18 @@ use Illuminate\Http\Request;
 
 class Auth0AuthenticateMiddleware
 {
+    public function __construct(
+        protected string $domain,
+        protected string $clientId,
+        protected string $cookieSecret,
+        protected string $audience,
+    ) {
+        Log::debug("Middleware Started with auth0 config", ["domain" => $this->domain, "clientId" => $this->clientId, "audience" => $this->audience]);
+    }
     public function handle(Request $request, Closure $next)
     {
        	Log::debug("Package HIT");
+
 	 // Add custom middleware logic here
         return $next($request);
     }
